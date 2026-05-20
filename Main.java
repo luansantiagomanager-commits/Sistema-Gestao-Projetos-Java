acd3124fbd4e2aa981594ba839187310c4930a5c
luansantiagomanager-commits <luansantiagomanager@gmail.com>
2026-05-20 16:15:59 -0300

    Add Main.java - Application entry point

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.manager;
+
+import com.manager.view.MenuConsole;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuConsole menu = new MenuConsole();
+        menu.iniciar();
+    }
+}
